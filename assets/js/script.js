54758c69197d4eeca5e76ffa3e231d956252eeb5
lukeinco <136262607+lukeinco@users.noreply.github.com>
2023-08-10 19:27:52 -0600

    basic js connections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-const url = 'https://movie-and-tv-shows-quotes.p.rapidapi.com/quotes/actor/al_pachino';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '777c9617ccmshf8277229fbc7775p143a73jsn252dbe9c9665',
-		'X-RapidAPI-Host': 'movie-and-tv-shows-quotes.p.rapidapi.com'
-	}
-};
+// const url = 'https://movie-and-tv-shows-quotes.p.rapidapi.com/quotes/actor/uma_thurman';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '777c9617ccmshf8277229fbc7775p143a73jsn252dbe9c9665',
+// 		'X-RapidAPI-Host': 'movie-and-tv-shows-quotes.p.rapidapi.com'
+// 	}
+// };
 
-fetch(url, options)
-.then(function(res) {
-    console.log(res.status)
-    return res.json();
-})
-.then(function(data) {
-    console.log(data)
-})
+// fetch(url, options)
+// .then(function(res) {
+//     console.log(res.status)
+//     return res.json();
+// })
+// .then(function(data) {
+//     console.log(data)
+// })
+
+var search = document.getElementById("search");
+var go = document.getElementById("go");
+go.addEventListener("click", );
